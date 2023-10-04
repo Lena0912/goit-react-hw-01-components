e75c8ddf7a4306aeb0438abca3fa87b9f5431566
Lena0912 <elenabelichenko9@gmail.com>
2023-10-04 18:57:54 +0300

    statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Profile } from 'components/Profile/Profile'
 import user from 'data/user.json'
+import { Statistics } from 'components/Statistics/Statistics'
+import data from 'data/data.json'
 export const App = () => {
   return (
     <div
@@ -18,7 +20,16 @@ export const App = () => {
   location={user.location}
   avatar={user.avatar}
   stats={user.stats}
-/>
+      />
+      <Statistics
+    title="Upload stats"
+    stats={data}
+  />
     </div>
+
+    
   );
 };
+
+  
+
